a87b74bc4b9ec1d4806d307a220ee8d8c6b30704
admin <admin@uscs-mac341.lan>
2022-07-26 14:54:54 -0700

    Adding highest peak Solution

---
+++ Prog_09_FindPeak.java
@@ -0,0 +1,22 @@
+public class Prog_09_FindPeak {
+    public int findPeakElement(int[] nums) {
+        int low =0;
+        int high = nums.length;
+        while(low<=high)
+        {
+            int mid = low + (high - low)/2;
+            if((mid == nums.length - 1 || nums[mid] > nums[mid+1]) && (mid == 0 || nums[mid] > nums[mid-1])) return mid; 
+            else if(mid > 0 && nums[mid-1] > nums[mid])
+            {
+                
+                high = mid -1;
+                
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
