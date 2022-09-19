867bffe80de74cbc246b534ca18034665676189e
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-09-18 21:13:17 -0700

    Time: 1 ms (64.82%), Space: 42.6 MB (9.52%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -14,34 +14,34 @@
  * }
  */
 class Solution {
-    TreeNode x_parent;
-        TreeNode y_parent;
-        int x_depth;
-        int y_depth;
         
     public boolean isCousins(TreeNode root, int x, int y) {
-        if(root == null) return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         
-        dfs(root,x,y,0,null);
-        
-        return x_depth == y_depth && x_parent != y_parent;
-    }
-    
-    private void dfs(TreeNode root, int x, int y, int depth, TreeNode parent){
-        
-        if(root == null) return;
-        
-        if(root.val == x)
-        {
-            x_parent = parent;
-             x_depth = depth;
+        while(!q.isEmpty()){
+            int size = q.size();
+            boolean x_found = false;
+            boolean y_found = false;
+            for(int i = 0; i<size ;i++){
+                 TreeNode curr = q.poll();
+                if(curr.val == x) x_found = true;
+                if(curr.val == y) y_found = true;
+                
+                if(curr.left != null && curr.right != null){
+                    if(curr.left.val == x && curr.right.val == y) return false;
+                    if(curr.right.val == x && curr.left.val == y) return false;
+                }
+                
+                
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+                
+                
+            }
+           if(x_found && y_found) return true;
+            if(x_found || y_found) return false;
         }
-        if(root.val == y){
-             y_parent = parent;
-             y_depth = depth;
-        }
-        
-        dfs(root.left, x,y, depth + 1, root);
-        dfs(root.right,x,y,depth + 1, root);
+        return true;
     }
 }
