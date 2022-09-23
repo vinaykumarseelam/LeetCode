e6b4093d2c3e39cc029ba9eda088a4688ec42c63
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-09-22 23:27:02 -0700

    Time: 48 ms (9.72%), Space: 42.5 MB (82.13%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,36 +1,33 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-       if(s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>(); 
+      
         
-        HashMap<Character, Character> sMap = new HashMap<> ();
-        HashMap<Character, Character> tMap = new HashMap<> ();
-        
-        for(int i=0 ; i < s.length() ; i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
+        for(int i = 0; i< s.length() ; i++){
+             char sChar = s.charAt(i);
+             char tChar = t.charAt(i);
             
-            if(sMap.containsKey(sChar))
-            {
-                if(sMap.get(sChar) != tChar) return false;
+            if(sMap.containsKey(sChar)){
+                if( sMap.get(sChar) != tChar) return false;
+                    
+                    
                 
-            } 
-            else
-            {
-                sMap.put(sChar, tChar);
+            }
+            else{
+                sMap.put(sChar,tChar);
             }
             
-            if(tMap.containsKey(tChar))
-            {
+            if(tMap.containsKey(tChar)){
                 if(tMap.get(tChar) != sChar) return false;
             }
             else
             {
-                tMap.put(tChar, sChar);
+                tMap.put(tChar,sChar);
             }
-            
         }
         return true;
-       
     }
     
 }
