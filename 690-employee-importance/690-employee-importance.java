50b32e88bc3df77c0324b2b77ebef27568a28da2
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-09-23 15:28:05 -0700

    Time: 10 ms (57.50%), Space: 57.9 MB (72.14%) - LeetHub

--- 690-employee-importance/690-employee-importance.java
+++ 690-employee-importance/690-employee-importance.java
@@ -9,27 +9,24 @@ class Employee {
 
 class Solution {
     HashMap<Integer, Employee> map;
+    int result;
     public int getImportance(List<Employee> employees, int id) {
         map = new HashMap<>();
         
-        for(Employee e : employees){
+        for(Employee e : employees)
+        {
             map.put(e.id, e);
         }
-        
-        int result =0;
-        
-        Queue<Integer> q = new LinkedList<>();
-        q.add(id);
-        
-        while(!q.isEmpty()){
-            int eid = q.poll();
-            Employee e = map.get(eid);
-                result += e.importance;
-            
-            for (int subId : e.subordinates){
-                q.add(subId);
-            }
-        }
+        dfs(id);
         return result;
     }
+     private void dfs(int id){
+         Employee e = map.get(id);
+         result += e.importance;
+         for(int subId : e.subordinates){
+             dfs(subId);
+         }
+         
+     }      
+    
 }
