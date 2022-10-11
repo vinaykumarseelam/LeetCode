842d6b019a106cc960f029e35a48ad95e2d20277
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-10-11 08:59:49 -0700

    Time: 2 ms (96.89%), Space: 42.9 MB (55.17%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -5,7 +5,8 @@ class Solution {
         if(grid == null || grid.length == 0) return 0;
        int m = grid.length;
        int n = grid[0].length;
-        dirs = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+        //dirs = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+         dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
        
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
@@ -30,7 +31,7 @@ class Solution {
     
     
     private void dfs(int[][] grid, int r, int c, int time, int m, int n){
-        int[][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
+        //int[][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
         //base
         if(r<0 || r == m || c<0 || c == n || grid[r][c] == 0) return;
         if(grid[r][c] != 1 && grid[r][c] < time) return;
