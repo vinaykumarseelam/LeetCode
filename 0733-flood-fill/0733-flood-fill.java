b41673534cce85798a7556b2b6b1d9ebcc7fa8ce
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-10-11 19:47:09 -0700

    Time: 2 ms (34.13%), Space: 48.2 MB (28.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,33 +6,30 @@ class Solution {
         if(image[sr][sc] == color) return image;
         int m = image.length;
         int n = image[0].length;
-        Queue<Integer> q = new LinkedList<>();
-        
-        q.add(sr);
-        q.add(sc);
         int oldcolor = image[sr][sc];
-        image[sr][sc] = color;
-         int[][] dirs = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int curr1 = q.poll();
-            int curr2 = q.poll();
-           
-            
-            for(int[] dir : dirs)
-            {
-                int nr = curr1 + dir[0];
-                int nc = curr2 + dir[1];
-                
-                if(nr>=0 && nc >=0 && nr < m && nc < n && image[nr][nc] == oldcolor){
-                    q.add(nr);
-                    q.add(nc);
-                    image[nr][nc] = color;
-                    
-                }
-            }
+        int[][] dirs = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
+        dfs(image,sr,sc,color,oldcolor,dirs); 
+        return image;
+
+             
             
         }
-        return image;
-       
+    
+    private void dfs(int[][] image, int r, int c, int color, int oldcolor, int[][] dirs){
+        
+        //base
+        if(r<0 || r == image.length || c<0 || c == image[0].length || image[r][c] != oldcolor) return;
+        
+        //code
+        image[r][c] = color;
+      
+        
+        for(int[] dir : dirs){
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            dfs(image,nr,nc,color,oldcolor,dirs);
+            
+        }
     }
-}
+        
+    }
