fd11adbaa5b0c38943c4d89ad198170f74eab93d
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-11-05 12:00:41 -0700

    Time: 4 ms (97.76%), Space: 42.3 MB (85.33%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,38 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> smap = new HashMap<>();
-        HashMap<Character, Character> tmap = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
+        char smap[] = new char[256];
+        char tmap[] = new char[256];
+        
+        for(int i = 0; i < s.length() ; i++){
             char schar = s.charAt(i);
             char tchar = t.charAt(i);
-            if(smap.containsKey(schar)){
-                if(smap.get(schar) != tchar) return false;
-                
+            
+            if(smap[schar-' '] != 0){ 
+                if(smap[schar - ' '] != tchar) return false;
             }
             else{
-                smap.put(schar,tchar);
+                smap[schar - ' '] = tchar;
             }
             
-            if(tmap.containsKey(tchar)){
-                if(tmap.get(tchar) != schar) return false;
+            if(tmap[tchar - ' '] != 0){
+                if(tmap[tchar - ' '] != schar) return false;
             }
             else{
-                tmap.put(tchar,schar);
+                tmap[tchar - ' '] = schar;
             }
         }
         return true;
     }
 }
+
+
+// 1 Approach - O(n), O(1)
+
+// Read question carefullt
+// We have to check in both directoins. So we are maintaining two hahsmaps
+// to remove a charcter from string using chatAt with index. Removing each character from two strings at a time and compoaring same timr.
+// Time Complexity is O(n). Space O(1).
+     
+// 2 Approach - O(n), O(1)
+     
+// Using Ascii code for storing values inside array.
